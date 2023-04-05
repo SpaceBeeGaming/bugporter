1dcd903563b24dc06e083a5585877403cab83a12
SpaceBeeGaming <space.bee.gaming@gmail.com>
2023-04-05 14:37:16 +0300

    Stub ReportBugFunction

--- Bugporter.API/Startup.cs
+++ Bugporter.API/Startup.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 [assembly: FunctionsStartup(typeof(Bugporter.API.Startup))]
 
@@ -10,15 +8,5 @@ public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
-        builder.Services.AddSingleton<HelloWorld>();
     }
-}
-
-public class HelloWorld
-{
-    private readonly ILogger<HelloWorld> _logger;
-
-    public HelloWorld(ILogger<HelloWorld> logger) => _logger = logger;
-
-    public void Run() => _logger.LogInformation("HelloWorld");
 }
